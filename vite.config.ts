29bbeeefe24f329d5918ae099ee299154b69a38b
Max.Chou <v2asault@yahoo.com.tw>
2025-10-23 17:55:59 +0800

    change eslint version and configure scss

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler'
+      }
+    }
+  },
   server: {
     proxy: {
       '/api': {
