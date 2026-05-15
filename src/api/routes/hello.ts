b524c598b0c80068e9b2893d3fedad88e43297ca
Max.Chou <v2asault@yahoo.com.tw>
2026-05-15 16:42:43 +0800

    volvo testing

--- src/api/routes/hello.ts
+++ src/api/routes/hello.ts
@@ -1,22 +1,41 @@
-import { zValidator } from '@hono/zod-validator';
-import { Hono } from 'hono';
-import { z } from 'zod';
+import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono";
+import { z } from "zod";
 
-const hello = new Hono().get(
-  '/hello',
+const hello = new Hono();
+
+hello.get(
+  "/hello",
   zValidator(
-    'query',
+    "query",
     z.object({
       name: z.string(),
     }),
   ),
   (c) => {
-    const { name } = c.req.valid('query');
+    const { name } = c.req.valid("query");
     return c.json({
       message: `Hello! ${name}`,
     });
   },
 );
+
+hello.get("/token", async (c) => {
+  const requestOptions = {
+    method: "GET",
+    redirect: "follow",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const res = await fetch(
+    "https://dmsuatapi.volvocartw.com/openapi/bff-sales-line/auth/token?lineId=1568126&userCode=KTA-max&password=Aa1234567&clientId=7761bedc-12e4-4768-b173-bb0c8e99e12f",
+    requestOptions as RequestInit,
+  );
+  const data = await res.json();
+  return c.json(data);
+});
 
 export type HelloController = typeof hello;
 export default hello;
