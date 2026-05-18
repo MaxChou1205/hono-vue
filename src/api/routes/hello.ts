43d15d3331a8972c80e65fb06469f5c06d11a42b
Max.Chou <v2asault@yahoo.com.tw>
2026-05-18 18:05:25 +0800

    volvo testing

--- src/api/routes/hello.ts
+++ src/api/routes/hello.ts
@@ -21,6 +21,9 @@ hello.get(
 );
 
 hello.get("/token", async (c) => {
+  const userCode = c.req.query("userCode") || "KTA-max";
+  const password = c.req.query("password") || "Aa1234567";
+
   const requestOptions = {
     method: "GET",
     redirect: "follow",
@@ -30,7 +33,7 @@ hello.get("/token", async (c) => {
   };
 
   const res = await fetch(
-    "https://dmsuatapi.volvocartw.com/openapi/bff-sales-line/auth/token?lineId=1568126&userCode=KTA-max&password=Aa1234567&clientId=7761bedc-12e4-4768-b173-bb0c8e99e12f",
+    `https://dmsuatapi.volvocartw.com/openapi/bff-sales-line/auth/token?lineId=1568126&userCode=${userCode}&password=${password}&clientId=7761bedc-12e4-4768-b173-bb0c8e99e12f`,
     requestOptions as RequestInit,
   );
   const data = await res.json();
